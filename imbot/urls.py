983560d9aa2348f136d83d61575ae04222d69bad
imbot1028 <imbot1028@gmail.com>
2022-06-11 14:45:20 +0900

    blog앱 추가

--- imbot/urls.py
+++ imbot/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from hello import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello', views.index),
+    path('hello/', include('hello.urls')),
 ]
